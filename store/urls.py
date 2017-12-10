01f29d82e925f28ce51b479c6e01e6f5f3b128cf
coryswainston <swainston.cory89@gmail.com>
2017-12-09 22:52:52 -0700

    Add all pages and media queries

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name="home"),
+    url(r'^shop/$', views.shop, name="shop"),
+    url(r'^cart/$', views.cart, name="cart"),
+    url(r'^checkout/$', views.checkout, name="checkout"),
+    url(r'^contact/$', views.contact, name="contact"),
+    url(r'^values/$', views.values, name="values"),
 ]
--- store/views.py
+++ store/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'store/base.html')
+
+def shop(request):
+    return render(request, 'store/shop.html')
+
+def cart(request):
+    return render(request, 'store/cart.html')
+
+def checkout(request):
+    return render(request, 'store/checkout.html')
+
+def contact(request):
+    return render(request, 'store/contact.html')
+
+def values(request):
+    return render(request, 'store/values.html')
